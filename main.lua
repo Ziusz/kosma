653a05d2aa0d49263b7ff5aa8c539a48cf73112c
Ziusz <podengo@tlen.pl>
2024-12-03 07:12:45 +0100

    Changed background color

--- main.lua
+++ main.lua
@@ -1,5 +1,8 @@
 function love.load()
-
+	local background = {}
+	background.r, background.g, background.b = love.math.colorFromBytes(21, 47, 157)
+	love.graphics.setBackgroundColor(background.r, background.g, background.b)
+	love.graphics.setDefaultFilter("nearest", "nearest")
 end
 
 function love.update()
