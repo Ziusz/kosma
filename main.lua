aadbf7cc507f58a952b425a4633d73af03c65834
Ziusz <podengo@tlen.pl>
2024-12-03 07:36:41 +0100

    Added simple functionality to create small circles when you click the LMB

--- main.lua
+++ main.lua
@@ -1,8 +1,11 @@
 function love.load()
+	love.graphics.setDefaultFilter("nearest", "nearest")
+
 	local background = {}
 	background.r, background.g, background.b = love.math.colorFromBytes(21, 47, 157)
 	love.graphics.setBackgroundColor(background.r, background.g, background.b)
-	love.graphics.setDefaultFilter("nearest", "nearest")
+
+	stars = {}
 end
 
 function love.update()
@@ -11,4 +14,13 @@ end
 
 function love.draw()
 	love.graphics.print("Hello, Kosma! :D", 10, 10)
+	for _, star in ipairs(stars) do
+		love.graphics.circle("fill", star.x, star.y, 5)
+	end
+end
+
+function love.mousepressed(x, y, button, istouch, presses)
+	if button == 1 then
+		table.insert(stars, {x = x, y = y})
+	end
 end
